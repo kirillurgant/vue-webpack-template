ab7cd7e30815041b008fa73c7b6b9a7e58043237
Kirill Urgant <etozhkirill@yandex.ru>
2018-07-01 17:38:01 +0300

    add style loader

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>hello</div>
+  <div class="text">{{ text }}</div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      text: "Hello"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+$red: red;
+
+.text {
+  color: $red;
+}
+</style>
