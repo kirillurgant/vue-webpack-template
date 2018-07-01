2d591fd08c99a2f01ce078c9e68881089a5a8b9c
Kirill Urgant <etozhkirill@yandex.ru>
2018-07-01 17:25:00 +0300

    add eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue';
-import App from './components/App.vue';
+import Vue from "vue";
+import App from "./components/App.vue";
 
-new Vue({
-  el: '#app',
-  render: h => h(App),
+export default new Vue({
+  el: "#app",
+  render: h => h(App)
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,39 @@
-const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 const PATHS = {
-  src: path.join(__dirname, 'src'),
-  dist: path.join(__dirname, 'dist'),
+  src: path.join(__dirname, "src"),
+  dist: path.join(__dirname, "dist")
 };
 
 module.exports = {
-  entry: path.join(PATHS.src, 'index.js'),
+  entry: path.join(PATHS.src, "index.js"),
   output: {
     path: PATHS.dist,
-    filename: 'bundle.js',
-    publicPath: 'dist',
+    filename: "bundle.js",
+    publicPath: "dist"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: "babel-loader"
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader"
       }
     ]
   },
-  plugins: [
-    new VueLoaderPlugin(),
-  ],
+  plugins: [new VueLoaderPlugin()],
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: "vue/dist/vue.esm.js"
     },
-    extensions: ['*', '.js', '.vue', '.json'],
+    extensions: ["*", ".js", ".vue", ".json"]
   },
   devServer: {
-    port: 3000,
-  },
+    port: 3000
+  }
 };
